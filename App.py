d41a49c06498e558d2484d9433430a502b224bc0
Charles Ewart <charlescewart@gmail.com>
2020-08-24 10:49:18 +1000

    integrating game into app gui

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from Game import *
 
 class RegionButton(tk.Frame):
     def __init__(self, parent, name, *args, **kwargs):
@@ -26,8 +26,8 @@ class App(tk.Frame):
         self.parent = parent
         parent.title("Diplomacy")
 
-        self.next_phase_btn = tk.Button(self.parent, text="Next phase", command=self.change_phase, image=pixel, height=20,
-                                   width=100, compound="c").grid(row=0, column=2)
+        self.next_phase_btn = tk.Button(self.parent, text="Next phase", command=self.change_phase, image=pixel,
+                                        height=20, width=100, compound="c").grid(row=0, column=2)
         self.phase_lbl = tk.Label(self.parent, text="Build", image=pixel, height=20, width=100, compound="c")
         self.phase_lbl.grid(row=0, column=1)
 
@@ -38,23 +38,6 @@ class App(tk.Frame):
         self.btn5 = RegionButton(root, "Liverpool").grid(row=2, column=1)
         self.btn6 = RegionButton(root, "North Sea").grid(row=2, column=2)
 
-        """self.btn1 = tk.Button(self.parent, text="Clyde", command=lambda row=1, column=0 : self.create_unit(row, column),
-                              anchor="n", image=pixel, height=100, width=100, compound="c").grid(row=1, column=0)
-        self.btn2 = tk.Button(self.parent, text="Edinburgh", command=lambda row=1, column=1 : self.create_unit(row, column),
-                              anchor="n", image=pixel, height=100, width=100, compound="c").grid(row=1, column=1)
-        self.btn3 = tk.Button(self.parent, text="Yorkshire", command=lambda row=2, column=0 : self.create_unit(row, column),
-                              anchor="n", image=pixel, height=100, width=100, compound="c").grid(row=2, column=0)
-        self.btn4 = tk.Button(self.parent, text="Liverpool", command=lambda row=2, column=1 : self.create_unit(row, column),
-                              anchor="n", image=pixel, height=100, width=100, compound="c").grid(row=2, column=1)
-        self.btn5 = tk.Button(self.parent, text="Norwegian Sea", command=lambda row=1, column=2 : self.create_unit(row, column),
-                              anchor="n", image=pixel, height=100, width=100, compound="c").grid(row=1, column=2)
-        self.btn6 = tk.Button(self.parent, text="North Sea", command=lambda row=2, column=2 : self.create_unit(row, column),
-                              anchor="n", image=pixel, height=100, width=100, compound="c").grid(row=2, column=2)"""
-
-    def create_unit(self, r, c):
-        widget = self.parent.grid_slaves(row=r, column=c)[0]
-        widget.configure(image=army_img)
-
     def change_phase(self):
         self.phase_lbl.configure(text="Orders")
 
@@ -62,6 +45,6 @@ class App(tk.Frame):
 if __name__ == "__main__":
     root = tk.Tk()
     pixel = tk.PhotoImage(width=1, height=1)
-    army_img = tk.PhotoImage(file=r"C:\Users\charl\Desktop\army.png")
+    army_img = tk.PhotoImage(file=r"army.png")
     App(root).grid()
     root.mainloop()
