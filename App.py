fe79227a374c965bd3bde960b8534bef4dca2771
Charles Ewart <charlescewart@gmail.com>
2020-08-27 16:54:36 +1000

    move unit from and to with buttons

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from Game import *
-
+from Orders import Order, CreateUnit
 
 class App(tk.Frame):
     def __init__(self, parent, game, *args, **kwargs):
@@ -16,20 +16,27 @@ class App(tk.Frame):
 
         self.print_phase_btn = tk.Button(self.parent, text="Print phase", command=self.print_phase, image=pixel,
                                          height=20, width=100, compound="c").grid(row=0, column=0)
+        self.print_orders_btn = tk.Button(self.parent, text="Print orders", command=self.print_orders, image=pixel,
+                                         height=20, width=100, compound="c").grid(row=1, column=0)
+        self.print_board_btn = tk.Button(self.parent, text="Print board", command=self.print_board, image=pixel,
+                                         height=20, width=100, compound="c").grid(row=1, column=1)
         self.next_phase_btn = tk.Button(self.parent, text="Next phase", command=self.change_phase, image=pixel,
                                         height=20, width=100, compound="c").grid(row=0, column=2)
 
         self.phase_lbl = tk.Label(self.parent, text=self.game.get_phase, image=pixel, height=20, width=100, compound="c")
         self.phase_lbl.grid(row=0, column=1)
 
-        self.btn1 = RegionButton(root, self.game, "Clyde").grid(row=1, column=0)
-        self.btn2 = RegionButton(root, self.game, "Edinburgh").grid(row=1, column=1)
-        self.btn3 = RegionButton(root, self.game, "Norwegian Sea").grid(row=1, column=2)
-        self.btn4 = RegionButton(root, self.game, "Yorkshire").grid(row=2, column=0)
-        self.btn5 = RegionButton(root, self.game, "Liverpool").grid(row=2, column=1)
-        self.btn6 = RegionButton(root, self.game, "North Sea").grid(row=2, column=2)
+        self.btn1 = RegionButton(root, self.game, "Clyde").grid(row=2, column=0)
+        self.btn2 = RegionButton(root, self.game, "Edinburgh").grid(row=2, column=1)
+        self.btn3 = RegionButton(root, self.game, "Norwegian Sea").grid(row=2, column=2)
+        self.btn4 = RegionButton(root, self.game, "Yorkshire").grid(row=3, column=0)
+        self.btn5 = RegionButton(root, self.game, "Liverpool").grid(row=3, column=1)
+        self.btn6 = RegionButton(root, self.game, "North Sea").grid(row=3, column=2)
 
     def change_phase(self):
+        if self.game.get_phase == "build":
+            self.game.collect_build_orders()
+            self.game.game_map.resolve_builds(self.game.order_sheet)
         self.game.next_phase()
         self.phase_lbl.configure(text=self.game.get_phase)
 
@@ -38,6 +45,12 @@ class App(tk.Frame):
 
     def print_phase(self):
         print(self.get_phase())
+
+    def print_orders(self):
+        print(game.order_sheet)
+
+    def print_board(self):
+        self.game.game_map.print_extended_board()
 
 
 class RegionButton(tk.Frame):
@@ -55,6 +68,8 @@ class RegionButton(tk.Frame):
         if not self.army:
             self.btn.configure(image=army_img)
             self.army = True
+            order = CreateUnit(0, self.name)
+            self.game.players[0].orders.append(order)
         else:
             self.btn.configure(image=pixel)
             self.army = False
@@ -64,10 +79,8 @@ class RegionButton(tk.Frame):
 
     def click_button(self):
         if self.game.get_phase == "build":
-            print("building unit")
             self.create_unit()
         elif self.game.get_phase == "orders":
-            print("moving unit")
             self.move_unit()
 
 
